5ba06ab5706740b0644e0ca5cac590be88d44dd0
Pxczxn <Pxczxn@163.com>
2026-07-28 04:26:47 +0800

    feat: add community report schema

---
+++ database/rollback/R030__rollback_m4_report_center.sql
@@ -0,0 +1,4 @@
+DROP TRIGGER IF EXISTS `community_report_event_prevent_delete`;
+DROP TRIGGER IF EXISTS `community_report_event_prevent_update`;
+DROP TABLE IF EXISTS `community_report_event`;
+DROP TABLE IF EXISTS `community_report`;
